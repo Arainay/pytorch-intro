acc9ab3f333fb00e408146a60bc81b56ff4a2ccc
arainay <arainay@gmail.com>
2020-02-15 17:26:55 +0800

    Updated batch_size

--- Flattener.py
+++ Flattener.py
@@ -3,6 +3,6 @@ import torch.nn as nn
 
 class Flattener(nn.Module):
     def forward(self, x):
-        batch_size, _ = x.shape
+        batch_size = x.shape[0]
 
         return x.view(batch_size, -1)
